13aea42cc4f5fb8308c2c8f091f00fa4223f663f
PercyWolfraim <xpace100@gmail.com>
2019-12-15 18:38:07 -0500

    añadido el scripta para clonar un elemento en la seccion de galeria

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ $('#contactBoton').click(function(e){
   e.preventDefault();
   alert("Las redes sociales estan en mantenimiento");
 })
+
+var images = $('.G-obra').clone();
+
+$('.G-obra').after(images);
