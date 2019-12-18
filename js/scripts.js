4dc8b96e4789029d2df1ceb9b5b0cddb6a164f24
PercyWolfraim <xpace100@gmail.com>
2019-12-17 21:02:24 -0500

    Empezando a añadir fadeIn y fadeOut en el index

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 $('#contactBoton').click(function(e){
   e.preventDefault();
   alert("Las redes sociales estan en mantenimiento");
-})
+});
 
 var images = $('.G-obra').clone();
 
 $('.G-obra').after(images);
+
+$('#Ejemplo3').remove();
+$('#Ejemplo4').remove();
+
+$('#FlechaD').click(function(){
+  $('#Ejemplo1').fadeOut('fast',function(){
+    $('#Ejemplo2').fadeIn();
+  });
+
+});
