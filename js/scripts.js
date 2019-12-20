10969373e6bd1695a8474e8ff651ff706681c711
PercyWolfraim <xpace100@gmail.com>
2019-12-19 22:58:50 -0500

    Otro ejemplo en el index

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,10 @@ var images = $('.G-obra').clone();
 
 $('.G-obra').after(images);
 
-$('#Ejemplo3').remove();
 $('#Ejemplo4').remove();
 
 $('#FlechaD').click(function(){
   $('#Ejemplo1').fadeOut('fast',function(){
     $('#Ejemplo2').fadeIn();
   });
-
 });
